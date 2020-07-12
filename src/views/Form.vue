4ff75a424a8e54e3988c7713fc14a68dae44bc29
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-07-12 16:09:57 +0900

    [FEATURE](Comment)Comment Copy

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -12,18 +12,18 @@
         <el-button>Cancel</el-button>
       </el-form-item>
     </el-form>
-    <div>
-      <p>{{ appearanceComment }}</p>
-      <p>{{ aromaComment }}</p>
-      <p>{{ tasteComment }}</p>
-      <p>{{ conclusionComment }}</p>
-    </div>
+    <template v-if="generatedComment !== ''">
+      <el-input type="textarea" v-model="generatedComment" />
+      <el-button type="primary" native-type="button" @click="copyComment"
+        >Copy</el-button
+      >
+    </template>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import { Form, FormItem, Button } from "element-ui";
+import { Button, Form, FormItem, Input } from "element-ui";
 
 // Types
 import { FormFields } from "@/types/types";
@@ -39,6 +39,7 @@ import Taste from "@/components/Taste.vue";
     "el-button": Button,
     "el-form-item": FormItem,
     "el-form": Form,
+    "el-input": Input,
     Appearance,
     Aroma,
     Conclusion,
@@ -71,34 +72,32 @@ export default class MainForm extends Vue {
     }
   }
 
-  private appearanceComment = "";
-  private aromaComment = "";
-  private tasteComment = "";
-  private conclusionComment = "";
+  private generatedComment = "";
 
   private buildComment() {
-    this.appearanceComment =
+    this.generatedComment =
+      // Appearance
       `清澄度は、${this.formInput.appearance?.clarity?.toString()}。` +
       `輝きは、${this.formInput.appearance?.shades?.toString()}。` +
       `色調は、${this.formInput.appearance?.tone?.toString()}。` +
       `濃淡は、${this.formInput.appearance?.brightness?.toString()}。` +
       `粘性は、${this.formInput.appearance?.viscosity?.toString()}。` +
-      `外観の印象は、${this.formInput.appearance?.appearanceImpression?.toString()}。`;
-    this.aromaComment =
+      `外観の印象は、${this.formInput.appearance?.appearanceImpression?.toString()}。` +
+      // Aroma
       `第一印象は、${this.formInput.aroma?.firstImpression?.toString()}。` +
       `果実は、${this.formInput.aroma?.fruit?.toString()}。` +
       `花/植物は、${this.formInput.aroma?.flower?.toString()}。` +
       `芳香/香辛料/化学物質は、${this.formInput.aroma?.spice?.toString()}。` +
-      `香りの印象は、${this.formInput.aroma?.aromaImpression?.toString()}。`;
-    this.tasteComment =
+      `香りの印象は、${this.formInput.aroma?.aromaImpression?.toString()}。` +
+      // Taste
       `アタックは、${this.formInput.taste?.attack?.toString()}。` +
       `甘みは、${this.formInput.taste?.sweetness?.toString()}。` +
       `酸味は、${this.formInput.taste?.acidity?.toString()}。` +
       `タンニンは、${this.formInput.taste?.tannin?.toString()}。` +
       `バランスは、${this.formInput.taste?.balance?.toString()}。` +
       `アルコールは、${this.formInput.taste?.alcohol?.toString()}。` +
-      `余韻は、${this.formInput.taste?.aftertaste?.toString()}。`;
-    this.conclusionComment =
+      `余韻は、${this.formInput.taste?.aftertaste?.toString()}。` +
+      // Conclusion
       `評価は、${this.formInput.conclusion?.evaluation?.toString()}。` +
       `適正温度は、${this.formInput.conclusion?.temperature?.toString()}。` +
       `グラスは、${this.formInput.conclusion?.glass?.toString()}。` +
@@ -106,6 +105,12 @@ export default class MainForm extends Vue {
       `収穫年は、${this.formInput.conclusion?.vintage?.toString()}。` +
       `生産国は、${this.formInput.conclusion?.country?.toString()}。` +
       `主なブドウ品種は、${this.formInput.conclusion?.grape?.toString()}。`;
+  }
+
+  private copyComment() {
+    const copyText = document.querySelector("textarea");
+    copyText?.select();
+    document.execCommand("copy");
   }
 }
 </script>
